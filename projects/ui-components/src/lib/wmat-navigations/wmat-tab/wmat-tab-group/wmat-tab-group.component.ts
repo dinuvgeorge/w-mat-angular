c098b94c85b3a19bfb1c2e9bfe3099e9fbc2e994
George Dinu <dinu.v.george@gmail.com>
2021-12-02 12:42:27 +0200

    fixed NG0100: ExpressionChangedAfterItHasBeenCheckedError: Expression has changed after it was checked. Previous value: 'false'. Current value: 'true' issue

--- projects/ui-components/src/lib/wmat-navigations/wmat-tab/wmat-tab-group/wmat-tab-group.component.ts
+++ projects/ui-components/src/lib/wmat-navigations/wmat-tab/wmat-tab-group/wmat-tab-group.component.ts
@@ -1,4 +1,4 @@
-import {Component, ContentChildren, QueryList} from '@angular/core';
+import {AfterViewChecked, ChangeDetectorRef, Component, ContentChildren, QueryList} from '@angular/core';
 import {WmatTabComponent} from "../wmat-tab/wmat-tab.component";
 
 
@@ -7,8 +7,15 @@ import {WmatTabComponent} from "../wmat-tab/wmat-tab.component";
   templateUrl: './wmat-tab-group.component.html',
   styleUrls: ['./wmat-tab-group.component.css']
 })
-export class WmatTabGroupComponent {
+export class WmatTabGroupComponent implements AfterViewChecked {
 
-  @ContentChildren(WmatTabComponent) tabItems: QueryList<WmatTabComponent> | undefined;
+  @ContentChildren(WmatTabComponent) tabItems: QueryList<WmatTabComponent> | undefined = undefined;
+
+  constructor(private cdref: ChangeDetectorRef) {
+  }
+
+  ngAfterViewChecked(): void {
+    this.cdref.detectChanges();
+  }
 
 }
